cef87a3230f4433bed05c8426a6ea2d73a0b815d
Luis Felipe Tapajos <lftapajos@gmail.com>
2017-11-10 20:00:43 -0200

    Project created

---
+++ OramaTest/OramaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OramaTest
+//
+//  Created by Luis Felipe Tapajos on 10/11/17.
+//  Copyright © 2017 Luis Felipe Tapajos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OramaTest/OramaTestTests/OramaTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  OramaTestTests.swift
+//  OramaTestTests
+//
+//  Created by Luis Felipe Tapajos on 10/11/17.
+//  Copyright © 2017 Luis Felipe Tapajos. All rights reserved.
+//
+
+import XCTest
+@testable import OramaTest
+
+class OramaTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
