1829843c4980c3cd01633381c0a93ac742d41e7b
Luis Felipe Tapajos <lftapajos@gmail.com>
2017-11-14 04:43:56 -0200

    Preparing Api

--- OramaTest/OramaTest/API.swift
+++ OramaTest/OramaTest/API.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class API {
     
@@ -14,29 +16,66 @@ class API {
         
         //let url = API_URL
         
-        let funds = [
-            Fund(simpleName: "Fundo 1", operabilityMinimum: 1500, fundRisk: "3"),
-            Fund(simpleName: "Fundo 2", operabilityMinimum: 1000, fundRisk: "2"),
-            Fund(simpleName: "Fundo 3", operabilityMinimum: 500, fundRisk: "1"),
-            Fund(simpleName: "Fundo 4", operabilityMinimum: 2000, fundRisk: "4"),
-            Fund(simpleName: "Fundo 5", operabilityMinimum: 4000, fundRisk: "5"),
-            Fund(simpleName: "Fundo 6", operabilityMinimum: 5000, fundRisk: "6")
-        ]
+        //var fundsApi : Array<Fund> = []
         
-        //Salva os fundos recuperados da API
-        Dao().saveFunds(funds)
+        var fundsApi : Array<Fund> = []
+        var detailApi : Array<FundDetail> = []
         
-        let detail = [
-            FundDetail(fullName: "Fundo de Investimento 1", initialDate: "01-12", strategyVideo: "image1.jpg", fundManagerDescription: "Fundo de investimento para carteira 1"),
-            FundDetail(fullName: "Fundo de Investimento 2", initialDate: "01-12", strategyVideo: "image2.jpg", fundManagerDescription: "Fundo de investimento para carteira 2"),
-            FundDetail(fullName: "Fundo de Investimento 3", initialDate: "03-12", strategyVideo: "image3.jpg", fundManagerDescription: "Fundo de investimento para carteira 3"),
-            FundDetail(fullName: "Fundo de Investimento 4", initialDate: "05-12", strategyVideo: "image4.jpg", fundManagerDescription: "Fundo de investimento para carteira 4"),
-            FundDetail(fullName: "Fundo de Investimento 5", initialDate: "10-12", strategyVideo: "image5.jpg", fundManagerDescription: "Fundo de investimento para carteira 5"),
-            FundDetail(fullName: "Fundo de Investimento 6", initialDate: "15-12", strategyVideo: "image6.jpg", fundManagerDescription: "Fundo de investimento para carteira 6"),
-            ]
-        
-        //Salva os detalhes dos fundos recuperados da API
-        Dao().saveDetailFunds(detail)
+        Alamofire.request(API_URL, method: .get)
+            .responseJSON { response in
+                if response.data != nil {
+                    let json = JSON(data: response.data!)
+                    
+                    //print("json ======> \(json[0])")
+                    
+                    for i in 0 ..< 6 {
+                        let simpleName = json[i]["simple_name"].stringValue
+                        let operability = json[i]["operability"]
+                            let minimum = operability["minimum_initial_application_amount"].doubleValue
+                        let fundRisk = json[i]["specification"]
+                            let risk = fundRisk["fund_risk_profile"]
+                                let name = risk["name"].stringValue
+                        let fullName = json[i]["full_name"].stringValue
+                        let initialDate = json[i]["initial_date"].stringValue
+                        let strategyVideo = json[i]["strategy_video"]
+                            let description = strategyVideo["description"].stringValue
+                            //let url = strategyVideo["url"].stringValue
+                        let fund_manager = json[i]["fund_manager"]
+                            let descriptionManager = fund_manager["description"].stringValue
+                        
+//                        print("\(i)")
+//                        print("simple_name ======> \(simpleName)")
+//                        print("operabilityMinimum ======> \(minimum)")
+//                        print("fundRisk ======> \(name)")
+ 
+//                        print("fullName ======> \(fullName)")
+//                        print("initialDate ======> \(initialDate)")
+//                        print("description ======> \(description)")
+//                        print("url ======> \(url)")
+//                        print("descriptionManager ======> \(descriptionManager)")
+                        
+                        
+                        fundsApi.append(Fund(
+                            simpleName: simpleName,
+                            operabilityMinimum: minimum,
+                            fundRisk: name)
+                        )
+
+                        detailApi.append(FundDetail(fullName: fullName,
+                            initialDate: initialDate,
+                            strategyVideo: description,
+                            fundManagerDescription: descriptionManager)
+                        )
+                        
+                    }
+                    
+                    //Salva os fundos recuperados da API
+                    Dao().saveFunds(fundsApi)
+                    
+                    //Salva os detalhes dos fundos recuperados da API
+                    Dao().saveDetailFunds(detailApi)
+                }
+        }
         
         return true
     }
---
+++ OramaTest/Pods/Target Support Files/Pods-OramaTest/Pods-OramaTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_OramaTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_OramaTestVersionString[];
+
